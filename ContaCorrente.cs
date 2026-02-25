b1265e9f26055ebadeb7f1cd7fe875bbaf3682e0
juliaroliveira9-cmyk <julia.r.oliveira9@aluno.senai.br>
2026-02-25 10:53:12 -0300

    Add files via upload

---
+++ ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FASE04
+{
+    class ContaCorrente
+    {               
+            public override void CalcularTarifa()
+            {
+                Saldo -= 15.00m;
+            }   
+    }
+}
